1e7ac230f32b7665bde1630ee10ba937f73cf662
Danillo Guimarães <danillo@danilloguimaraes.com.br>
2019-01-06 20:19:24 -0200

    Configuração de roteamento

--- angular-med-store/src/app/app.module.ts
+++ angular-med-store/src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MaterialModule } from './material-module/material-module.module';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CarrinhoIndicadorComponent } from './viewmodel/carrinho-indicador/carrinho-indicador.component';
 import { ModelModule } from './model';
+
+const appRoutes: Routes = [
+  { path: '', component: AppComponent }
+];
 
 @NgModule({
   declarations: [
@@ -17,6 +22,10 @@ import { ModelModule } from './model';
     AppRoutingModule,
     ModelModule,
     MaterialModule,
+    RouterModule.forRoot(
+      appRoutes,
+      //{ enableTracing: true } // <-- Para fins de debug
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
