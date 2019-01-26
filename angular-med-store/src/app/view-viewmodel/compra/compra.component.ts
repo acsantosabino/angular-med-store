d59d3a7ca552225a0daf279bc2550929347bff10
Fanny Fanely <40525222+netfanely@users.noreply.github.com>
2019-01-26 13:55:46 -0200

    Add files via upload
    atualizar compra

--- angular-med-store/src/app/view-viewmodel/compra/compra.component.ts
+++ angular-med-store/src/app/view-viewmodel/compra/compra.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CarrinhoCompras, Compra } from 'src/app/model';
 
 
 @Component({
@@ -8,11 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CompraComponent implements OnInit {
 
+  public totalComprado: number;
+  public listaComprado: Compra[];
+  public readonly displayedColumns: string[] = [
+    'Produto',
+    'PrecoUnit',
+    'Quantidade',
+    'Total'];
+
   constructor(
+    protected readonly carrinho: CarrinhoCompras
     ) {
    }
 
+   calculaTotal() {
+     this.totalComprado = 0;
+     for (const compra of this.listaComprado) {
+       this.totalComprado += compra.total;
+     }
+   }
+
   ngOnInit() {
+    this.listaComprado = this.carrinho.getListaCompras();
+    console.log(this.listaComprado);
+    this.calculaTotal();
+    this.carrinho.finalizarCompra();
+    console.log(this.listaComprado);
   }
 
 }
